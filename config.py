ff25e5439880f7704bb312fa1926c65dec725e24
Rohan Awhad <rohanavad007@gmail.com>
2023-12-05 19:07:52 -0700

    Update config.py with new search and passage parameters

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 import torch
-from encoders import mpnet_encoder
+from encoders import mpnet_encoder, msmarco_distilbert_cos
 
 torch.set_grad_enabled(False)
 
-SEARCH_K = 3
-PASSAGE_K = 15
-BOOK_DIR = 'books'
-EMBD_DIR = 'embeddings'
-QUESTION_FILE = 'question.txt'
-PROMPT_TEMPLATE = 'prompt.template'
-PROMPT_FILE = 'prompt.txt'
-MAX_LEN = 1200
+SEARCH_K = 15
+PASSAGE_K = 75
+BOOK_DIR = "books"
+EMBD_DIR = "embeddings"
+QUESTION_FILE = "question.txt"
+PROMPT_TEMPLATE = "prompt.template"
+PROMPT_FILE = "prompt.txt"
+MAX_LEN = 500
 
 # encoder config
+# ENCODER = msmarco_distilbert_cos
 ENCODER = mpnet_encoder
